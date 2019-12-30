a7b465e91ffd7b91282b590bd5173d1049d40c26
Abhishek Kumar Verma <40899651+v-abhi@users.noreply.github.com>
2019-12-31 02:47:26 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,19 +1,16 @@
-# web-scrapping-jobsites
-It scarps job details of many companies and store in a single csv file. It saves time and resources.
-
-
-
 JOB_SCRAPPER is basically a PYTHON programe/script.
+
 It extract JOB_DETAILS like Job_Name, Url, Salary, Location, Eligibility, Experience, Position and Advertisement_Date of many Companies.
+
 These extracted data are saved into a CSV_File for better Analysis.
 
 
 
 
 OLD_METHODS
-  For searching about job vacancies, we have to search on Google or other Websites. We are given links about many companies.
-  We have to open all job_links and gather their job_details one by one. We have to remember previous job_details while shifting on
-  other job_links and while comparing among them. Or we have to pen down their details.
+    For searching about job vacancies, we have to search on Google or other Websites. We are given links about many companies.
+    We have to open all job_links and gather their job_details one by one. We have to remember previous job_details while shifting on
+    other job_links and while comparing among them. Or we have to pen down their details.
     
     It CONSUME SO MUCH TIME in LOADING_WEBPAGES, PENNING_DOWN_INFOs and SO MANY CLICKS.
     It ALSO REQUIRE so much CPU_Time and RAM and STRONG NETWORK.
@@ -22,20 +19,58 @@ OLD_METHODS
 
 
 NEW_METHODS
-  Using this programe/script, we can gather ALL these job_details about different jobs in A SINGLE CSV_File.
+    Using this programe/script, we can gather ALL these job_details about different jobs in A SINGLE CSV_File.
     
-    IT JUST TAKES 1-2 MINUTES in gathering Job_Details of approx 40 COMPANIES and STORING in CSV_File. 1-2 MINUTES VS 10-15 MINS.
+    IT JUST TAKES 1-2 MINUTES in gathering Job_Details of approx 40 COMPANIES and STORING in CSV_File. 1-2 MINUTES VS 10-15 MINUTES.
     IT CONSUMES VERY LITTLE CPU_TIME as well as RAM and WORKS in LOW NETWORK too.
     NO NEED OF BROWSER. 
     It becomes much easier to analyse among them BCZ ALL DATAS ARE GIVEN IN SYSTEMATIC WAY i.e. ROWS and COLUMNS.
+    
   
   
-  LIBRARIES/TECHNOLOGIES/LANGUAGES USED :
+    
+    
+    
+    
+    LIBRARIES/TECHNOLOGIES/LANGUAGES USED :
         PYTHON3
         ANT TEXT EDITOR
         any EXCEL programe that can handle CSV file
     
     
-        LIBRARIES : 
-            bs4 
-            request
+        LIBRARIES : bs4 and requests
+            if not installed 
+            install them
+            contact google
+        
+    
+        
+    HOW TO USE : 
+        download a file named "job_scrapper.zip"
+        unzip or extract files from this
+        
+        open "command prompt" or "terminal" in windows and ubuntu respectivellyg
+        go to the folder in which files are located i.e. in folder named "job_scrapper"
+        
+        run the python programe named "job_details_scrapper.py"
+            command :
+                python3 job_details_scrapper.py
+    
+    
+    
+    
+  
+    RESULTS :
+    
+        after 1-2 minutes after completion of this programe
+        
+        A CSV FILE named "job_file.csv" will be created containing details
+        
+        
+        
+        
+        
+    REMARKS :
+        ## working on Ubuntu
+        ## Hope works on others too.
+        
---
+++ job_details_scrapper.py
@@ -0,0 +1,147 @@
+import requests
+import bs4 
+import sys
+import re
+import os
+
+
+
+
+
+def in_csv_file(dictfile) :
+    list1 = ["Url", "Name", "Location", "Position", "Salary", "Experience", "Eligibility", "Date"]
+    #print (dictfile)
+    for i in range (0,8) :
+        if list1[i] in dictfile:    
+            temp1 = str(dictfile[list1[i]]).replace(",", " / ").replace("\n", " ").replace(":", "").replace(";", "/")    
+            f.write(temp1 + ",")    
+        else :
+            f.write("No Data" + ",")    
+    f.write("\n")    
+        
+    
+
+
+
+
+
+
+def company_scrapper(name,company_url,Date) :
+    response = requests.get(company_url)    
+    web_content = response.content    
+    soup = bs4.BeautifulSoup(web_content, 'html.parser')    
+    job_details = {}   
+    job_details["Url"]= company_url    
+    for job_description in soup.find_all("div", {"class" : "td-post-content"}) :    
+        for details in (list(job_description.find_all("strong")) + list(job_description.find_all("b"))) :   
+            if (re.search("^[a-zA-Z].*ompa.*", details.text.replace(u'\xa0', u' '))) :     
+                Name = details.next_sibling    
+                Name = str(Name).replace(u'\xa0', u' ')   
+                job_details["Name"]= name   
+            elif (re.search("^[a-zA-Z].*itio.*", details.text.replace(u'\xa0', u' '))) :
+                Position = details.next_sibling
+                Position = str(Position).replace(u'\xa0', u' ')
+                job_details["Position"]= Position
+            elif (re.search("^[a-zA-Z].*lar.*|ackag.*", details.text.replace(u'\xa0', u' '))) :
+                Salary = details.next_sibling
+                Salary = (str(Salary).replace(u'\xa0', u' '))
+                job_details["Salary"]= Salary
+            elif (re.search("^[a-zA-Z].*rienc.*", details.text.replace(u'\xa0', u' '))) :
+                Experience = details.next_sibling
+                Experience = str(Experience).replace(u'\xa0', u' ')
+                job_details["Experience"]= Experience
+            elif (re.search("Job Location", details.text.replace(u'\xa0', u' '))) :
+                Location = details.next_sibling
+                Location = str(Location).replace(u'\xa0', u' ')
+                job_details["Location"]= Location
+            elif (re.search("^[a-zA-Z].*gibil.*", details.text.replace(u'\xa0', u' '))) :
+                Eligibility = details.find_next().li      
+                try :
+                    Eligibility = Eligibility.text
+                    Eligibility = str(Eligibility).replace(u'\xa0', u' ')
+                    job_details["Eligibility"]= Eligibility
+                    break
+                except :
+                    a =0
+        break
+    job_details["Date"]= Date
+    in_csv_file(job_details)
+
+
+
+
+
+
+def link_scrapper(soup) :
+    for job_header in soup.findAll("h3", {"class" : "entry-title td-module-title"}) :    
+        for link in job_header.find_all('a') :    
+            Name = link.get('title')    
+            Url = link.get('href')
+            Date = link.find_next().time    
+            Date = Date.text   
+            company_scrapper(Name,Url,Date)   
+           
+    
+
+
+
+ 
+
+def scrapping_start() :
+    for page in range(1,2) :
+        url = "https://apuzz.com/tag/2019-batch/page/"+str(page)+"/"
+        response = requests.get(url)
+        web_content = response.content  
+        soup = bs4.BeautifulSoup(web_content, 'html.parser')    
+        link_scrapper(soup)   
+    
+  
+  
+  
+  
+
+
+temp0 = 0 
+def check() :
+    global temp0   
+    try :
+        response = requests.get('https://apuzz.com/tag/2019-batch/', timeout=10)    
+    except :
+        temp0 = 1
+        print ("NO INTERNET or OTHER PROBLEM\n")
+
+     
+
+
+
+
+
+file_name = "job_file.csv"
+file_path = os.getcwd()     
+if os.path.exists(file_path+"/"+file_name) :     
+    os.remove(file_path+"/"+file_name)
+f = open(file_name, "w", encoding='utf-8-sig')   
+headers = "Url, Name, Location, Position, Salary, Experience, Eligibility, Date\n"
+f.write(headers)    
+
+
+
+
+
+
+
+check()
+if temp0 == 0 :
+    scrapping_start()
+
+  
+f.close()
+            
+            
+            
+            
+            
+            
+            
+            
+            
